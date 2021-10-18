923dd581a6284d4a0bee79ea346ebe8f78949090
wennroy <wennroy@gmail.com>
2021-10-18 16:16:48 -0500

    Update GAM_Analysis

--- Code/Trial1.R
+++ Code/Trial1.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
 # Read the data ----------------------------------------
-# setwd("D:/WISC/stat628/Module 2/project")
+setwd("D:/WISC/stat628/Module 2/Stat628Module2_Group9")
 BodyFatData <- read.csv('../BodyFat.csv')
 
 del_index = c(33, 39, 42, 48, 76, 96, 163, 172, 182, 216, 221)
@@ -38,6 +38,10 @@ MSE_NR6_train = numeric(K) # Nonparametirc model with 3 variables
 MSE_NR6_test = numeric(K) # Nonparametirc model with 3 variables
 MSE_NR3_train = numeric(K) # Nonparametirc model with 6 variables
 MSE_NR3_test = numeric(K) # Nonparametirc model with 6 variables
+MSE_NRVS_train = numeric(K) # Nonparametirc model with 8 variables but not all variables are significant
+MSE_NRVS_test = numeric(K)
+MSE_NRVS2_train = numeric(K) # Nonparametirc model with 5 variables. NR6 - FOREARM.
+MSE_NRVS2_test = numeric(K)
 
 library(mgcv)
 library(leaps)
@@ -68,27 +72,61 @@ for (i in c(1:K)){
   best_num_variables = which.min(bic)
   lm_best_index = which(ss$which[best_num_variables,][2:15])
   fit_best = lm(Y~., data = cbind(Y,X[,lm_best_index]))
+  print(fit_best$rank)
   # summary(fit_best)
   pred_y = predict(fit_best, newdata = BodyFat_test[,(lm_best_index+1)])
   MSE_lm_train[i] = rss[best_num_variables] / (n) # For easier to compare, set the denominator to be n.
   MSE_lm_test[i] = cal_MSE(BodyFat_test$BODYFAT, pred_y)
   
-  fit_sm6 <- gam(BODYFAT~ 1 + s(AGE) + WEIGHT + s(ABDOMEN) + s(THIGH) + FOREARM + s(WRIST),data = BodyFat_train)
+  fit_sm6 <- mgcv::gam(BODYFAT~ 1 + AGE + WEIGHT
+                       + s(ABDOMEN)
+                       + s(THIGH)
+                       + FOREARM + s(WRIST)
+                       ,data = BodyFat_train, method = 'REML')
   # summary(fit_sm6)
   pred_y = predict(fit_sm6,newdata = BodyFat_test)
   MSE_NR6_train[i] = sum(fit_sm6$residuals)/n
   MSE_NR6_test[i] = cal_MSE(BodyFat_test$BODYFAT, pred_y)
   
-  fit_sm3 <- gam(BODYFAT~ 1 + WEIGHT + s(ABDOMEN)+ s(WRIST),data = BodyFat_train)
+  fit_sm3 <- mgcv::gam(BODYFAT~ 1 + WEIGHT + s(ABDOMEN)+ s(WRIST),data = BodyFat_train,method = 'REML')
   # summary(fit_sm3)
   pred_y = predict(fit_sm3,newdata = BodyFat_test)
   MSE_NR3_train[i] = sum(fit_sm3$residuals)/n
   MSE_NR3_test[i] = cal_MSE(BodyFat_test$BODYFAT, pred_y)
+  
+  fit_sm_nx_lm <- mgcv::gam(BODYFAT~ 1 + AGE + WEIGHT
+                            + s(ABDOMEN)
+                            + s(THIGH)
+                            + s(WRIST)
+                            ,data = BodyFat_train, method = 'REML')
+  # summary(fit_sm_nx_lm)
+  pred_y = predict(fit_sm_nx_lm,newdata = BodyFat_test)
+  MSE_NRVS_train[i] = sum(fit_sm_nx_lm$residuals)/n
+  MSE_NRVS_test[i] = cal_MSE(BodyFat_test$BODYFAT, pred_y)
+  
+  fit_sm_nx_lm2 <- mgcv::gam(BODYFAT~ 1 + AGE  + WEIGHT
+                             + s(NECK)  + s(ABDOMEN)
+                             + s(THIGH) + s(BICEPS)
+                             + FOREARM + s(WRIST)
+                             ,data = BodyFat_train, method = 'REML')
+  # summary(fit_sm_nx_lm2)
+  pred_y = predict(fit_sm_nx_lm2,newdata = BodyFat_test)
+  MSE_NRVS2_train[i] = sum(fit_sm_nx_lm2$residuals)/n
+  MSE_NRVS2_test[i] = cal_MSE(BodyFat_test$BODYFAT, pred_y)
 }
-
 
 
 (result = data.frame(lm_test = mean(MSE_lm_test),lm_train = mean(MSE_lm_train),
                     NR3_test = mean(MSE_NR3_test), NR3_train = mean(MSE_NR3_train),
-                    NR6_test = mean(MSE_NR6_test), NR6_train = mean(MSE_NR6_train)))
+                    NR6_test = mean(MSE_NR6_test), NR6_train = mean(MSE_NR6_train),
+                    NR_VS_test = mean(MSE_NRVS_test), MSE_NRVS_train = mean(MSE_NRVS_train),
+                    NR_VS2_test = mean(MSE_NRVS2_test), MSE_NRVS2_train = mean(MSE_NRVS2_train)))
 print(result)
+
+(result_rmse = data.frame(lm_test = sqrt(mean(MSE_lm_test)),lm_train = sqrt(mean(MSE_lm_train)),
+                     NR3_test = sqrt(mean(MSE_NR3_test)), NR3_train = sqrt(mean(MSE_NR3_train)),
+                     NR6_test = sqrt(mean(MSE_NR6_test)), NR6_train = sqrt(mean(MSE_NR6_train)),
+                     NR_VS_test = sqrt(mean(MSE_NRVS_test)), MSE_NRVS_train = sqrt(mean(MSE_NRVS_train)),
+                     NR_VS2_test = sqrt(mean(MSE_NRVS2_test)), MSE_NRVS2_train = sqrt(mean(MSE_NRVS2_train))))
+
+print(result_rmse)
